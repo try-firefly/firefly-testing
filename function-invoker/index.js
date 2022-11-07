36c43490199bea039dc71a31e8caaa62325beec3
Marcus Sinkinson <sinkinsonmarcus@gmail.com>
2022-11-07 23:15:45 +0100

    index.js update
    Co-authored-by: wyennie <wryennie@gmail.com>

--- function-invoker/index.js
+++ function-invoker/index.js
@@ -1,10 +1,6 @@
 const axios = require('axios');
 
-const FAAS_URLS = [
-  "https://4c7mxmu2mvpnjugwjkrubznksu0tzgrc.lambda-url.eu-central-1.on.aws/",
-  "https://ok44be5zia2dea3nuzfsllf3mq0bgnhs.lambda-url.eu-central-1.on.aws/",
-  "https://2whoo2hwsk7how5crbuyyj5zb40bsnqp.lambda-url.eu-central-1.on.aws/",
-];
+const FAAS_URLS = [];
 
 const INTERVAL = 1000; // this is the rate we call setTimeout
 const VARIANCE = 10000; // this is the max range of the random time (in milliseconds) setTimeout will wait
